3f9fd61537e956269fcbd31d26e1734a5947a6e5
Jordan Glass <jordanglass21@gmail.com>
2025-01-18 17:53:07 -0500

    everything working

--- test/bankTests.js
+++ test/bankTests.js
@@ -5,6 +5,8 @@ var accounts = [{ id: 1234567890, balance: 5000 },
     { id: 1234567891, balance: 10000 }];
 var usernames = ['user1', 'user2'];
 var bank = new bank_1.Bank(accounts, usernames);
+// user story 1
+console.log("------------------User Story 1------------------");
 // Scenario 1: successful account created
 var acc = bank.createAccount('user1', 20, 1234567892);
 if (acc.id !== 1234567892
@@ -14,13 +16,16 @@ if (acc.id !== 1234567892
 }
 else {
     console.log('Scenario 1 passed');
+    bank.printAccount(acc.id); // display results
 }
+// Scenario 1: account not created because user id is not unique
 try {
     bank.createAccount('user1', 20, 1234567892);
     console.log('Scenario 1 failed');
 }
 catch (e) {
     console.log('Scenario 1 passed');
+    console.log(e.message);
 }
 // scenario 2: unsuccessful account creation due to customer being below 18
 try {
@@ -30,6 +35,12 @@ try {
 catch (e) {
     console.log('Scenario 2 passed');
 }
+try { // display results
+    bank.printAccount(1234567899);
+}
+catch (e) {
+    console.log(e.message);
+}
 // Scenario 3: unsuccessful account creation due to invalid username
 try {
     bank.createAccount('user3', 20, 1234567888);
@@ -37,4 +48,104 @@ try {
 }
 catch (e) {
     console.log('Scenario 3 passed');
+}
+try { // display results
+    bank.printAccount(1234567888);
+}
+catch (e) {
+    console.log(e.message);
+}
+// user story 2
+console.log("------------------User Story 2------------------");
+// Scenario 1: customer is able to deposit money into an account
+try {
+    var balance = bank.deposit(1234567892, 100);
+    if (balance === 100) {
+        console.log('Scenario 1 passed');
+        bank.printAccount(1234567892); // display results
+    }
+    else {
+        console.log('Scenario 1 failed');
+    }
+}
+catch (e) {
+    console.log('Scenario 1 failed');
+    console.log(e.message);
+}
+// Scenario 2: customer is unable to deposit money into bank account becuase
+// the bank account does not exist
+try {
+    bank.deposit(1299999990, 100);
+    console.log('Scenario 2 failed');
+}
+catch (e) {
+    console.log('Scenario 2 passed');
+    console.log(e.message);
+}
+// Scenario 3: customer is unable to deposit money into bank account because
+// the indicated deposit amount is negative
+try {
+    bank.deposit(1234567890, -100);
+    console.log('Scenario 3 failed');
+}
+catch (e) {
+    console.log('Scenario 3 passed');
+    console.log(e.message);
+}
+// user story 3
+console.log("------------------User Story 3------------------");
+// Scenario 1: customer is able to withdraw money from an account
+try {
+    var balance = bank.withdraw(1234567892, 100);
+    if (balance === 0) {
+        console.log('Scenario 1 passed');
+        bank.printAccount(1234567892);
+    }
+    else {
+        console.log('Scenario 1 failed');
+    }
+}
+catch (e) {
+    console.log('Scenario 1 failed');
+}
+// Scenario 2: customer is unable to withdraw money from bank account because
+// the bank account does not exist
+try {
+    bank.withdraw(1299999990, 100);
+    console.log('Scenario 2 failed');
+}
+catch (e) {
+    console.log('Scenario 2 passed');
+    console.log(e.message);
+}
+// Scenario 3: customer is unable to withdraw money from their bank account because
+// the indicated withdrawal amount is greater than their account balance
+try {
+    bank.withdraw(1234567890, -1000000000);
+    console.log('Scenario 3 failed');
+}
+catch (e) {
+    console.log('Scenario 3 passed');
+    console.log(e.message);
+}
+// user story 4
+console.log("------------------User Story 4------------------");
+// Scenario 1: customer is able to check the balance of an account
+try {
+    var balance = bank.checkBalance(1234567892);
+    console.log('Scenario 1 passed');
+    console.log("Balance: $%d", balance);
+}
+catch (e) {
+    console.log('Scenario 1 failed');
+}
+// Scenario 2: customer is unable to check the balance of a bank account because
+// the bank account does not exist
+try {
+    bank.checkBalance(1299999990);
+    console.log('Scenario 2 failed');
+}
+catch (e) {
+    console.log('Scenario 2 passed');
+    console.log(e.message);
 }
